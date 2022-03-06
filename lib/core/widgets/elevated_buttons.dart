83bb529400fd597cabe52d04d835c358c9751125
Jahongir Anvarov <95640875+JaxaAnvarov@users.noreply.github.com>
2022-03-06 23:05:56 +0500

    elevated buttonga dizayn berildi

--- lib/core/widgets/elevated_buttons.dart
+++ lib/core/widgets/elevated_buttons.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:foody/core/constants/size_config.dart';
 
 class DefaultButtons extends StatelessWidget {
-  DefaultButtons({Key? key}) : super(key: key);
+  DefaultButtons(
+      {Key? key,
+      required this.onPressed,
+      required this.child,
+      this.height,
+      this.radius,
+      this.primaryColor,
+      this.width})
+      : super(key: key);
 
   VoidCallback onPressed;
   Widget child;
+  late Color? primaryColor;
+  late double? radius;
+  late double? width;
+  late double? height;
 
   @override
   Widget build(BuildContext context) {
@@ -12,12 +25,21 @@ class DefaultButtons extends StatelessWidget {
       style: _style(),
       child: child,
       onPressed: onPressed,
-
     );
   }
 
-  _style(){
-    return ElevatedButton.styleFrom();
+  _style() {
+    return ElevatedButton.styleFrom(
+      primary: primaryColor,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(
+          getProportionateScreenWidth(radius!),
+        ),
+      ),
+      fixedSize: Size(
+        getProportionateScreenWidth(width!),
+        getProportionateScreenHeight(height!),
+      ),
+    );
   }
-
 }
